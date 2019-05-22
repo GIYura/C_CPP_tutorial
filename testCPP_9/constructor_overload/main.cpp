6dc0913a7d6dc9775566dedd7b78df08f70b99f8
jura <jglotovyn@gmail.com>
2019-05-22 23:51:21 +0300

    commit

--- testCPP_9/constructor_overload/main.cpp
+++ testCPP_9/constructor_overload/main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
+#include <ctime>
 
 using namespace std;
 
+class Timer{
+    int seconds;
+public:
+    Timer(char *t) { seconds = atoi(t); }
+    Timer(int t) { seconds = t; }
+    Timer(int min, int sec) { seconds = min * 60 + sec; }
+    void run();
+};
+
+void Timer::run(){
+    clock_t time;
+    time = clock();
+    while( (clock() / CLOCKS_PER_SEC - time / CLOCKS_PER_SEC) < seconds);
+    cout << "\a";
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    Timer a(10), b("20"), c(1, 10);
+    a.run();
+    b.run();
+    c.run();
     return 0;
 }
